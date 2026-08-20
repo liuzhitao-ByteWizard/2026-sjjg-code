10103b3f14d31b1887935a3f03281b53270bcf27
byte-wizard <liuzhitao2026@163.com>
2026-08-20 20:33:07 +0800

    11

---
+++ 2026_8_19/2026_8_19/main.c
@@ -0,0 +1,136 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <assert.h>
+//#include <stdbool.h>
+//
+//struct opPD {
+//    char op;  // 运算符
+//    int pd;   // 优先级
+//};
+//
+//int operatorPrecedence(char ch) {
+//    // '('的优先级需要很低，方便'('后面的下一个运算符入栈
+//    static struct opPD arr[5] = {
+//        {'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}, {'(', 0}
+//    };
+//
+//    for (int i = 0; i < 5; ++i) {
+//        if (arr[i].op == ch) {
+//            return arr[i].pd;
+//        }
+//    }
+//
+//    // 如果获取优先级运算符不是以上运算符则报错
+//    assert(false);
+//    return -1;
+//}
+//
+//// 判断是否是操作数
+//bool isOperand(const char* str) {
+//    // 只有一个字符，且是+-*/等运算符
+//    if (str[0] >= '0' && str[0] <= '9')
+//        return true;
+//    else if (str[0] == '-' && str[1] >= '0' && str[1] <= '9')
+//        return true;
+//
+//    return false;
+//}
+//
+//void toRPN(const char* tokens[], int tokensSize) {
+//    // 假设tokensSize<=10000
+//    // 所以这里我们直接创建一个10000静态数组做栈
+//    char stackArr[10000];
+//
+//    // top标记栈顶数据下一个位置
+//    int top = 0;
+//
+//    for (int i = 0; i < tokensSize;) {
+//        const char* str = tokens[i];
+//
+//        if (isOperand(str)) {  // 操作数
+//            // 操作数直接输出
+//            printf("%s ", str);
+//            ++i;
+//        }
+//        else {  // 界限符
+//            if (str[0] == '(') {
+//                // 左括号入栈
+//                stackArr[top++] = str[0];
+//                ++i;
+//            }
+//            else if (str[0] == ')') {
+//                // 右括号，则将所有的栈中的运算符全部出栈，直到遇到左括号
+//                while (stackArr[top - 1] != '(') {
+//                    printf("%c ", stackArr[--top]);
+//                }
+//
+//                --top;  // 删除左括号
+//                ++i;
+//            }
+//            else {
+//                // 运算符
+//                // 1、如果栈为空或者栈不为空且当前运算符比栈顶运算符优先级高，则当前运算符入栈
+//                // 2、如果栈不为空且比栈顶运算符优先级低或相等，说明栈顶的运算符可以运算了，
+//                // 输出栈顶运算符，当前运算符循环一圈，走运算法判断的逻辑。
+//                if (top == 0 ||
+//                    operatorPrecedence(str[0]) >
+//                    operatorPrecedence(stackArr[top - 1])) {
+//                    stackArr[top++] = str[0];
+//                    ++i;
+//                }
+//                else {
+//                    printf("%c ", stackArr[--top]);
+//                }
+//            }
+//        }
+//    }
+//
+//    // 栈中的运算符全部输出
+//    while (top != 0) {
+//        printf("%c ", stackArr[--top]);
+//    }
+//}
+//
+//int main()
+//{
+//    //"1+2-3";
+//    //const char* expression[] = { "1", "+", "2", "-", "3" };
+//
+//    //"1+2-(3*4+5)-6";
+//    const char* expression[] = {
+//        "1", "+", "2", "-", "(", "3", "*", "4", "+", "5", ")", "-", "6"
+//    };
+//
+//    //"1+2-(3*(4+5))-6";
+//    //const char* expression[] = {
+//    //    "1", "+", "2", "-", "(", "3", "*", "(", "4", "+", "5", ")", ")", "-", "6"
+//    //};
+//
+//    toRPN(expression, sizeof(expression) / sizeof(expression[0]));
+//
+//    return 0;
+//}
+
+#include <stdio.h>
+
+int main() {
+    // 定义3行4列的二维数组A
+    int A[3][4] = { {1, 2, 3, 4},
+                   {5, 6, 7, 8},
+                   {9, 10, 11, 12} };
+
+    // C语言二维数组就是按照行优先存储
+    // 下标第2行、第3列的数据是12，下面我们直接打印它的地址
+    printf("%p\n", &A[2][3]);
+
+    // Type A[M][N]的二维数组，假设二维数组的起始地址为start，
+    // 则A[i][j]的存储地址 = start + (i * n + j) * sizeof(Type)
+
+    // 这里我们按照行优先存储公式，由首地址计算12的地址，跟上一句是一样的
+    // 需要额外说明的是&A[0][0]的类型是int*，int*变量加1是加一个
+    // int大小，即4 byte，所以这里没有乘以sizeof(int)
+    printf("%p\n", &A[0][0] + 2 * 4 + 3);
+
+    return 0;
+}
+
---
+++ 2026_8_20/2026_8_20/main.c
@@ -0,0 +1,144 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+//int sumOfLeftLeaves(struct TreeNode* root) {
+//    // 如果root为NULL，则直接返回0
+//    if (root == NULL)
+//        return 0;
+//
+//    int sum = 0;
+//
+//    // 如果左子树根节点不为空，且左子树根节点的左右子树均为空，则进行结果累加
+//    if (root->left && root->left->left == NULL && root->left->right == NULL)
+//        sum += root->left->val;
+//
+//    // 递归遍历查找左右子树中，满足左叶子节点性质的节点，并进行累加
+//    sum += sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+//
+//    // 返回sum结果，递归过程中是向上层交付计算结果
+//    return sum;
+//}
+
+// 判断两棵树是否相同：
+//bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+//    if (p == NULL && q == NULL)
+//        return true;
+//
+//    if (p == NULL || q == NULL)
+//        return false;
+//
+//    // 如果p和q两个节点值相等，则继续递归判断左右子树中两棵树的节点值是否相等
+//    if (p->val == q->val)
+//        return isSameTree(p->left, q->left) &&
+//        isSameTree(p->right, q->right);
+//
+//    return false;
+//}
+//
+//
+//// 判断是否为另一棵树的子树：
+//bool isSubtree(struct TreeNode* root, struct TreeNode* subroot) {
+//    if (root == NULL)
+//        return false;
+//
+//    if (isSameTree(root, subroot))
+//        return true;
+//
+//    // 递归判断左右子树和root是否相等，其中一棵树与root相等，
+//    // 整个结果都为true，所以这里是或的关系
+//    return isSubtree(root->left, subroot) ||
+//        isSubtree(root->right, subroot);
+//}
+
+//bool chkPalindrome(ListNode* A) {
+//    // 如果A为空，或只有一个节点，则直接返回true
+//    if (A == NULL || A->next == NULL)
+//        return true;
+//
+//    ListNode* slow, * fast;
+//    slow = fast = A;
+//
+//    // 1. 通过快慢指针的方法，找到链表中间节点
+//    while (fast && fast->next)
+//    {
+//        // 慢指针走一步，快指针走两步
+//        slow = slow->next;
+//        fast = fast->next->next;
+//    }
+//
+//    // 2. 链表后半部分逆置，三指针的方式实现
+//    ListNode* prev, * cur, * nxt;
+//    prev = NULL;
+//    cur = slow;
+//
+//    while (cur)
+//    {
+//        // 记录cur的下一个节点
+//        nxt = cur->next;
+//
+//        // 实现反转，将cur的next指向prev前一个节点
+//        cur->next = prev;
+//
+//        // 更新prev和cur，以此向后进行遍历反转
+//        prev = cur;
+//        cur = nxt;
+//    }
+//
+//    // 3. 逐节点进行比对
+//    while (A && prev)
+//    {
+//        // 如果两个节点值不相等，则直接返回false
+//        if (A->val != prev->val)
+//            return false;
+//
+//        // 更新指针，继续向后遍历
+//        A = A->next;
+//        prev = prev->next;
+//    }
+//
+//    return true;
+//}
+
+void _MergeSort(int* arr, int begin, int end , int* tmp) {
+	if (begin >= end)
+		return;
+
+	int mid = begin + (end - begin) / 2;
+	//[begin mid] [mid + 1 , end]
+	
+	//处理左区间
+	_MergeSort(arr, begin, mid, tmp);
+	//处理右区间
+	_MergeSort(arr, mid + 1, end, tmp);
+
+	int begin1 = begin, begin2 = mid + 1;
+	int end1 = mid, end2 = end;
+	
+	int i = begin;
+
+	while (begin1 < end1 && begin2 < end2) {
+		if (arr[begin1] <= arr[begin2])
+			tmp[i++] = arr[begin1];
+		else
+			tmp[i++] = arr[begin2];
+	}
+
+	while (begin1 <= end1) {
+		tmp[i++] = arr[begin1];
+	}
+
+	while (begin2 <= end2) {
+		tmp[i++] = arr[begin2];
+	}
+
+	//将排序好的子区间拷贝回原数组
+	for (int i = begin; i <= end;i++) {
+		arr[i] = tmp[i];
+	}
+}
+
+void MergeSort(int* arr, int n) {
+	int* tmp = (int*)malloc(sizeof(int) * n);
+	int begin = 0;
+	int end = n - 1;
+	_MergeSort(arr, begin , end , tmp);
+	free(tmp);
+}
